159653d1008c8e20a137cc856e747ac9c7a537ee
kristjanjansen <kristjan.jansen@gmail.com>
2016-03-05 17:27:44 +0200

    aaa

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -22,7 +22,7 @@ class LoginController extends Controller
 
         if ($this->signIn($request)) {
             return redirect()
-                ->route('frontpage.index')
+                ->route('styleguide')
                 ->with('info', trans('auth.login.login.info'));
         }
 
