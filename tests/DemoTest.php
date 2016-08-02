31ba26069960e1930f8afedf811630ed7869f0c6
kristjanjansen <kristjan.jansen@gmail.com>
2016-08-02 11:38:59 +0300

    hmm

--- tests/DemoTest.php
+++ tests/DemoTest.php
@@ -12,22 +12,36 @@ class DemoTest extends TestCase
     public function test_forum_content_do_not_update_timestamp_on_update()
     {
     
+        $regular_user = factory(App\User::class)->create();
+
+        $type = 'forum';
+
         $content = factory(Content::class)->create([
-            'user_id' => factory(App\User::class)->create()->id,
-            'type' => 'forum'
+            'user_id' => $regular_user->id,
+            'type' => $type
         ]);
 
         $first_date = Content::find($content->id)->updated_at;
         
+        /*
         sleep(2);
 
         $content->update(['title' => 'Updated']);
 
         $second_date = Content::find($content->id)->updated_at;
 
-//        $this->assertTrue($first_date->gt($second_date));
+        $this->assertGreaterThan($first_date->timestamp, $second_date->timestamp);
+        */
+
+        $this->actingAs($regular_user)
+            ->visit("content/$type/$content->id/edit")
+            ->type("Hola titulo", 'title')
+            ->press(trans('content.edit.submit.title'));
+
+        $second_date = Content::find($content->id)->updated_at;
 
         $this->assertGreaterThan($first_date->timestamp, $second_date->timestamp);
+        
     }
 
 }
